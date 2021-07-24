0de583647e7b97fc8c30fb2ac0bfdf40729bbbe6
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2021-07-24 17:38:07 +0200

    Add invariant checks for search cache tests

--- cache_test.go
+++ cache_test.go
@@ -26,6 +26,69 @@ func copyByteSlice(slc []byte) []byte {
 	return cpy
 }
 
+func checkInvariants(cache *SearchCache) bool {
+	for index, search := range cache.keySlice {
+		var inActiveRange bool
+		if cache.keyEndIndex == cache.keyStartIndex {
+			inActiveRange = false
+		} else if cache.keyEndIndex > cache.keyStartIndex {
+			inActiveRange = index >= cache.keyStartIndex && index < cache.keyEndIndex
+		} else {
+			inActiveRange = index >= cache.keyStartIndex || index < cache.keyEndIndex
+		}
+
+		// Check if the result that is supposed to be stored actually is
+		if inActiveRange {
+			if cache.Fetch(search) == nil {
+				return false
+			}
+		}
+
+		var storedCount int
+		if cache.keyEndIndex == cache.keyStartIndex {
+			storedCount = 0
+		} else if cache.keyEndIndex > cache.keyStartIndex {
+			storedCount = cache.keyEndIndex - cache.keyStartIndex
+		} else {
+			storedCount = cache.keyEndIndex + len(cache.keySlice) - cache.keyStartIndex
+		}
+
+		// Check if the number of stored searches corresponds with the number of results
+		if storedCount != len(cache.resultData) {
+			return false
+		}
+
+		// Check if the key slice has not grown exceedingly
+		if len(cache.keySlice) > 3*storedCount {
+			return false
+		}
+
+		var realSize int
+		for _, result := range cache.resultData {
+			realSize += len(result)
+		}
+
+		// Check if the cache size is accurate
+		if realSize != cache.curByteSize {
+			return false
+		}
+
+		// Check if the maximum size is not exceeded
+		if cache.curByteSize > cache.maxByteSize {
+			return false
+		}
+
+		// Check if indices are valid
+		if cache.keyStartIndex >= len(cache.keySlice) {
+			return false
+		}
+		if cache.keyEndIndex > len(cache.keySlice) {
+			return false
+		}
+	}
+	return true
+}
+
 func TestSearchCacheStandard(t *testing.T) {
 	cache, _ := NewSearchCache(128)
 
@@ -75,6 +138,9 @@ func TestSearchCacheLarge(t *testing.T) {
 	}
 	for index := range searches {
 		cache.Store(searches[index], copyByteSlice(results[index]))
+		if !checkInvariants(cache) {
+			t.Fatal("Invariant violated")
+		}
 	}
 	for index := range searches {
 		if !bytes.Equal(cache.Fetch(searches[index]), results[index]) {
@@ -84,12 +150,16 @@ func TestSearchCacheLarge(t *testing.T) {
 }
 
 func TestSearchCacheHammer(t *testing.T) {
-	max := 12288
-	size := 8388608
-	count := 8096
-	cache, _ := NewSearchCache(size)
-	for i := 0; i < count; i++ {
-		cache.Store(randomSearch(), randomByteSlice(rand.Intn(max)))
+	cacheSize := 1048576
+	maxResultSize := 8192
+	storeCount := 2048
+	cache, _ := NewSearchCache(cacheSize)
+
+	for i := 0; i < storeCount; i++ {
+		if !checkInvariants(cache) {
+			t.Fatal("Invariant violated")
+		}
+		cache.Store(randomSearch(), randomByteSlice(rand.Intn(maxResultSize)))
 	}
 }
 
